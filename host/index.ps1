19467031296dc85981baebc7aeeac2924b37db41
jalal <jalal@superusers.dk>
2018-11-12 08:29:55 +0100

    remove-module psreadline

--- host/index.ps1
+++ host/index.ps1
@@ -1,7 +1,9 @@
-
-
 function env { ls env:\ }
 
 function cls { clear-host }
 function c   { clear-host }
 
+
+## Fjerne den gule farvet prompt  
+# $host.PrivateData.ErrorBackgroundColor = 15
+# remove-module psreadline
