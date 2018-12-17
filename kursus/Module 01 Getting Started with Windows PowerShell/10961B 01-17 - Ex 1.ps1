8461d17a7b86845a8a07567ec72176a7502945bd
dev <dev@superusers.dk>
2018-12-17 13:08:19 +0100

    '.'

--- kursus/Module 01 Getting Started with Windows PowerShell/10961B 01-17 - Ex 1.ps1
+++ kursus/Module 01 Getting Started with Windows PowerShell/10961B 01-17 - Ex 1.ps1
@@ -4,7 +4,7 @@
 # kræver at der internet forbindelse, og i nogle tilfælde at man har administrator rettighedder
 # for kunne installerer opdateringer til alle mouduler.
 
-Update-Help
+Update-Help -ErrorAction SilentlyContinue  -WarningAction SilentlyContinue  -Force
 
 # Hvis man ønsker at hente og gemme opdaterede hjælp filer, så de evt. kan overføres 
 # til maskiner som ikke har internet adgang.
