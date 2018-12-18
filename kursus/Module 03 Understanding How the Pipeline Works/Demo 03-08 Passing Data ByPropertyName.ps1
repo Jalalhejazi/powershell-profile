07a1cbc12580fd47d97e61051aa9cc70a72c169c
dev <dev@superusers.dk>
2018-12-18 12:00:21 +0100

    '.'

--- kursus/Module 03 Understanding How the Pipeline Works/Demo 03-08 Passing Data ByPropertyName.ps1
+++ kursus/Module 03 Understanding How the Pipeline Works/Demo 03-08 Passing Data ByPropertyName.ps1
@@ -2,7 +2,7 @@
 # execute just that line. 
 
 # 1
-Import-CSV E:\Mod03\Democode\Users1.csv
+Import-CSV .\Users1.csv
 # Notice that the CSV columns become properties, and
 # the rows become individual objects
 
@@ -13,20 +13,20 @@ Help New-ADUser –ShowWindow
 # pipeline input ByPropertyName
 
 # 3
-Import-CSV E:\Mod03\Democode\Users1.csv | New-ADUser –WhatIf
+Import-CSV .\Users1.csv | New-ADUser –WhatIf
 # This command would work, but does not represent a real-world
 # scenario. In a real organization, the people providing the
 # CSV file might not get the format correct.
 
 # 4
-Import-CSV E:\Mod03\Democode\Users2.csv
+Import-CSV .\Users2.csv
 # This is a more real-world input file. The samAccountName
 # property is missing. Perhaps a well-meaning person 
 # did not understand what it was for, so they removed it.
 # We need that to make the command work.
 
 # 5
-Import-CSV E:\Mod03\Democode\Users2.csv | Select-Object –Property *,@{n='samAccountName';e={$_.Name}} | New-ADUser
+Import-CSV .\Users2.csv | Select-Object –Property *,@{n='samAccountName';e={$_.Name}} | New-ADUser
 # This command will work. We modify the objects in the
 # pipeline to correspond with the parameter names we need.
 
---
+++ kursus/Module 03 Understanding How the Pipeline Works/demo/get-KomputerInfo.ps1
@@ -0,0 +1,49 @@
+﻿Clear-Host
+
+# finde en liste af computerNavne
+# Hent information om disse computers
+# sendes videre til andre PS functions
+  
+# get-content ./pc.txt     |
+# Get-KomputerInfo  
+
+### Liste af ComputerNames
+
+## "Offline", "localhost" , "127.0.0.1" , $env:COMPUTERNAME | Out-File ./pc.txt
+
+$data = Get-Content .\pc.txt 
+
+
+function Get-KomputerInfo() 
+{
+    # For Each Input from PIPELINE
+    process{
+        # Write-Output  "PC = $_"
+
+        ## How to get more info about Computer data
+        $results1 = gwmi -Query "SELECT * FROM win32_PingStatus Where Address ='$_' " 
+        
+        ##$results2 = gwmi -Query "SELECT * FROM class-software Where Address ='$_' "
+
+        $obj = New-Object psObject
+
+        $obj | Add-Member NoteProperty Navn $results1.PSComputerName
+        $obj | Add-Member NoteProperty IPv4 $results1.IPV4Address
+        $obj | Add-Member NoteProperty IPv6 $results1.IPV6Address
+        
+
+          # statuscode = 0 is good news; anything else is bad
+        if ($results1.statuscode -eq 0){
+                $obj| Add-Member NoteProperty Responding $true
+        }else {
+            $obj| Add-Member NoteProperty Responding $false
+        }     
+            Write-Output $obj
+    }
+}
+
+
+$data | 
+Get-KomputerInfo  | 
+select *
+
