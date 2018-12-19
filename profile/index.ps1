e1abf8f488a1758e74a8192cda6e62b91d82b6d2
dev <dev@superusers.dk>
2018-12-19 14:52:59 +0100

    '.'

--- profile/index.ps1
+++ profile/index.ps1
@@ -1,3 +1,8 @@
+## Add Modules to env:PSModulePath
+$env:PSModulePath = $env:PSModulePath + ";$ProfileHome/Modules/"
+
+
+
 function profile-edit   { subl $profilehome }
 function profile-reload { . $profile }
 
