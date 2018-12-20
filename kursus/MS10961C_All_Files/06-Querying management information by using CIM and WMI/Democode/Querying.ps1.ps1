e95ad990784f58ae7f85938227d8cda7157a5326
dev <dev@superusers.dk>
2018-12-20 12:05:57 +0100

    '.'

--- kursus/MS10961C_All_Files/06-Querying management information by using CIM and WMI/Democode/Querying.ps1.ps1
+++ kursus/MS10961C_All_Files/06-Querying management information by using CIM and WMI/Democode/Querying.ps1.ps1
@@ -11,7 +11,7 @@ Get-CimInstance –ClassName Win32_Process
 Get-CimInstance –ClassName Win32_LogicalDisk –Filter "DriveType=3"
 
 # 4
-Get-CimInstance –Query “SELECT * FROM Win32_NetworkAdapter"
+Get-CimInstance –Query "SELECT * FROM Win32_NetworkAdapter"
 
 
 
---
+++ kursus/MS10961C_All_Files/06-Querying management information by using CIM and WMI/readme.md
@@ -0,0 +1,10 @@
+# WMI Exporer
+
+## Source code
+
+* https://github.com/vinaypamnani/wmie2
+
+
+## Realeases
+
+* https://github.com/vinaypamnani/wmie2/releases
