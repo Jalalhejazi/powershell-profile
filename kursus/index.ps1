6d42cb72372c3f1dc7c80b6a6e637653c9f23b2a
dev <dev@superusers.dk>
2019-04-03 15:42:54 +0200

    '.'

--- kursus/index.ps1
+++ kursus/index.ps1
@@ -1,6 +1,16 @@
+
+<# 
+
+    To use node as commandline, you can use https://www.npmjs.com/package/commander
+
+    npm repo commander
+#>
+
+
 function ng-generate-component {
     param(
         $navn 
     )
    ng generate component $navn  --module=app.module.ts --export --no-spec --selector=$navn $args
 } 
+
