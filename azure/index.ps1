67fdc8de7dd714aaf1df3a73b9e5cf772599cdaf
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-06-14 12:44:50 +0200

    .

--- azure/index.ps1
+++ azure/index.ps1
@@ -1,11 +1,10 @@
 
 $subscription="MSDN_950_kr"
 $keyvault="superusers2020"
+
 az account set -s $subscription
 
 . "$ProfileHOME\azure\secrets.ps1"
 . "$ProfileHOME\azure\vm-connect.ps1"
-. "$ProfileHOME\azure\arm.ps1"
-. "$ProfileHOME\azure\webapp-snippets.ps1"
 . "$ProfileHOME\azure\redis.ps1"
 
