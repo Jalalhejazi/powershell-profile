c440582c2e406231bf0b16828ac18ff3b69fd089
dev <dev@superusers.dk>
2019-06-18 16:11:08 +0200

    .

--- json/read-data.ps1
+++ json/read-data.ps1
@@ -1,8 +1,16 @@
+<#
+    1. Læser indholdet af en json fil fra harddisken
+    2. Convert fra json til powershell object
+    3. foreach --> loop for hvert række i en array
+    4. select kolonner fra alle niveauer 
+#>
+
+# data kan også komme fra internet via REST API Services
 $data = Get-Content './data.json' | ConvertFrom-Json 
 
 foreach ($x in $data) {
     $x | Select-Object  personNavn, 
                         personTitel , 
-                        @{Expression={$PSItem.adresse.vejnavn}; Name="vejNavn"},
-                        @{Expression={$PSItem.adresse.postNummer}; Name="postNummer"}
+                        @{Expression={$PSItem.adresse.vejnavn + ' ' + $PSItem.adresse.husNummer}; Name="vejNavn"},
+                        @{Expression={$PSItem.adresse.postNummer}  ; Name="postNummer"}
 } 
