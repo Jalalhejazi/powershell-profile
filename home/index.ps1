256017a91ab667088c12339985175db6ac1433bf
dev <dev@superusers.dk>
2019-06-20 16:21:38 +0200

    .

--- home/index.ps1
+++ home/index.ps1
@@ -23,12 +23,5 @@ function goto-temp  {
 }
 
 
-
-## Create New-PSDrive K pointing to Kursus
-## New-PSDrive -Name "K" -PSProvider "FileSystem" -Root "$ProfileHOME/kursus" 
-
-## Create New-PSDrive E pointing to Allfiles
-## New-PSDrive -Name "E" -PSProvider "FileSystem" -Root "$ProfileHOME/kursus/MS10961C_All_Files" 
-
-goto-dev
+goto-home
 
