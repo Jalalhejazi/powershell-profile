12a93f70a941507af1149fa627c9fdd9484c14d4
dev <dev@superusers.dk>
2019-06-21 10:14:23 +0200

    .

--- prompt/index.ps1
+++ prompt/index.ps1
@@ -3,8 +3,9 @@ function prompt
 {
     $temperatur = get-temperatur-dk 
     $IP = get-private-IP-address
-   
-    $host.UI.RawUI.WindowTitle = "$pwd    -    $temperatur";
+    $time = "{0,2:hh}:{0,2:mm}" -f (Get-Date)
+
+    $host.UI.RawUI.WindowTitle = "$pwd    -    $temperatur    -    time ($time)";
    
     return "$IP $ "
 }
