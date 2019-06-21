ef3b9f88ba6af68052622efb8e13306b16840b5d
dev <dev@superusers.dk>
2019-06-21 10:25:38 +0200

    .

--- prompt/index.ps1
+++ prompt/index.ps1
@@ -1,9 +1,16 @@
-
 function prompt
 {
     $temperatur = get-temperatur-dk 
     $IP = get-private-IP-address
     $time = "{0,2:hh}:{0,2:mm}" -f (Get-Date)
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
+    $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+    if ($isAdmin) {
+        Write-Host ("ADMIN ") -nonewline -foregroundcolor DarkYellow -backgroundcolor DarkBlue
+        $leftCharCount += 1
+    }
+
 
     $host.UI.RawUI.WindowTitle = "$pwd    -    $temperatur    -    time ($time)";
    
