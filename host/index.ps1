e11dffc50f862efba1dae22b6ec34b5a582b2b8f
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-06-27 12:12:49 +0200

    .

--- host/index.ps1
+++ host/index.ps1
@@ -9,6 +9,28 @@ function host-set-layout-mocha {
     concfg  import mocha --yes --non-interactive
 }
 
+function host-set-layout-monokai {
+    # scoop install concfg
+    # concfg  presets
+    concfg  import monokai --yes --non-interactive
+}
+
+
+
+function host-set-layout-google-light {
+    # scoop install concfg
+    # concfg  presets
+    concfg  import google-light --yes --non-interactive
+}
+
+function host-set-layout {
+    # scoop install concfg
+    # concfg  presets
+    param($layoutName)
+    concfg  import $layoutName --yes --non-interactive
+}
+
+
 
 function host-list-layout {
     # scoop install concfg
