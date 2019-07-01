35a0b2cb26615fb47e41a37d1803c811f77f5cd4
dev <dev@superusers.dk>
2019-07-01 10:29:44 +0200

    .

--- kursus/kursus_webapp/index.ps1
+++ kursus/kursus_webapp/index.ps1
@@ -6,3 +6,20 @@ function ng-generate-component {
    ng generate component $navn  --module=app.module.ts --export --no-spec --selector=$navn $args
 } 
 
+function ng-new-app {
+<#
+
+.EXAMPLE
+
+ng-new-app demo -d
+
+.EXAMPLE
+ng-new-app demo -d
+
+
+#>
+    param($AppName)
+
+    goto-dev
+    ng new $AppName --minimal --routing  --defaults $args 
+}
