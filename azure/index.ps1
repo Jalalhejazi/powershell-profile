315bc69683e3dfef4c51434d17eae81a55635377
Jalal Hejazi <jalal.hejazi@superusers.dk>
2019-08-26 14:14:10 +0200

    conditional loading azuer subscription

--- azure/index.ps1
+++ azure/index.ps1
@@ -1,8 +1,15 @@
 
-#$subscription="MSDN_950_kr"
-#$keyvault="superusers2020"
-#az account set -s $subscription
+$author = 'jalal'
 
+if ($env:USERNAME -eq $author) {
+      
+    $subscription="MSDN_950_kr"
+    $keyvault="superusers2020"
+    az account set -s $subscription
+    
+    Clear-Host
+    Write-Host "Loading Azure $subscription subscription and $keyvault secrets for user $author "
+}
 
 . "$ProfileHOME\azure\secrets.ps1"
 . "$ProfileHOME\azure\vm-connect.ps1"
