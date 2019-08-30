4f9817fb4fa93cd95555a9da5c53e722daef0012
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-08-30 11:56:25 +0200

    '.'

--- git/git-deploy.ps1
+++ git/git-deploy.ps1
@@ -1,6 +1,6 @@
 function deploy  { npm run deploy    }
 function deploy-feature  { npm run deploy-feature  }
-function deploy-broken  { npm run deploy-feature  }
+function deploy-broken  { npm run deploy-broken  }
 function deploy-status  { npm run deploy-status  }
 
 function gs    { git status        }
