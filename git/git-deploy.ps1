543aa4d8f55ac5ad613fad4694013d333d457cfd
Jalal Hejazi <jalal.hejazi@gmail.com>
2019-10-25 14:41:35 +0000

    Updated git-deploy.ps1

--- git/git-deploy.ps1
+++ git/git-deploy.ps1
@@ -3,6 +3,16 @@ function deploy-feature  { npm run deploy-feature  }
 function deploy-broken  { npm run deploy-broken  }
 function deploy-status  { npm run deploy-status  }
 
-function gs       { git status        }
-function git-log  { git log --graph --oneline --decorate}
+function gs       { git status }
 
+function git-config-alias {
+    git config --global alias.g git
+    git config --global alias.s status
+    git config --global alias.d diff
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.last "log -1 HEAD"
+    git config --global alias.cmt "commit --amend --no-edit"
+    git config --global alias.pr "pull --rebase"
+    git config --global alias.log "log --graph --oneline --decorate -n 30"
+}
