14947f47b773d1247c38adf330b1ea2c54296fd6
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-07-01 07:29:00 +0000

    Updated vm-connect.ps1

--- azure/vm-connect.ps1
+++ azure/vm-connect.ps1
@@ -10,3 +10,17 @@ $port   = "3389"
 Start-Process "mstsc" -ArgumentList "/V:$server`:$port /fullscreen /admin " 
 }
 
+
+
+
+function create-vm-linux{
+    param(
+        $rgName="vm2020",
+        $VMName="dev2020"
+    )
+    $secret = get-azure-secret sysadmin
+    az group create --location westus --name $rgName
+    az vm create --resource-group $rgName --name $VMName --image Debian --admin-username sysadmin --admin-password $secret --tags "env=training"
+}
+
+
