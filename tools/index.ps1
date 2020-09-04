ce0da941b992b61392bb7339e330921c19cf158b
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-09-04 12:20:41 +0000

    Updated index.ps1

--- tools/index.ps1
+++ tools/index.ps1
@@ -78,5 +78,14 @@ function please-make-my-life-easy {
 }
 
 
+## vscode extensions TODO
+
+## npm i -g azure-functions-core-tools@3 --unsafe-perm true
+## code --install-extension ms-vscode.csharp 
+## code --install-extension ms-vscode.azurecli
+## code --install-extension ms-azuretools.vscode-azurefunctions
+## code --install-extension ms-vscode.azure-account
+## code --install-extension ms-azuretools.vscode-azurestorage
+## code --install-extension ms-vscode.powershell 
 
 
