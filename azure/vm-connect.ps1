253743284b7d91224edafff7d37a8745367c2d7a
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-09-09 19:47:58 +0000

    Updated vm-connect.ps1

--- azure/vm-connect.ps1
+++ azure/vm-connect.ps1
@@ -1,26 +1,8 @@
 
-function connect-win2016-dev-pc {
-<#
-    change "windows-nano-server-on-azure" to your Azure VM Name
-#>
-
-$server = "windows-nano-server-on-azure.northeurope.cloudapp.azure.com"
+function connect-dev-pc {
+$server = "dev2020.westeurope.cloudapp.azure.com"
 $port   = "3389" 
 
 Start-Process "mstsc" -ArgumentList "/V:$server`:$port /fullscreen /admin " 
 }
-
-
-
-
-function create-vm-linux{
-    param(
-        $rgName="vm2020",
-        $VMName="dev2020"
-    )
-    $secret = get-azure-secret sysadmin
-    az group create --location westus --name $rgName
-    az vm create --resource-group $rgName --name $VMName --image Debian --admin-username sysadmin --admin-password $secret --tags "env=training"
-}
-
 
