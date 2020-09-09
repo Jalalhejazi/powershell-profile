54582ffd84548c000f6bd750009de8bb54165b63
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-09-09 17:24:32 +0000

    Updated index.ps1

--- env/index.ps1
+++ env/index.ps1
@@ -3,24 +3,24 @@ function env-list { dir env:\$args }
 
 function env-set {
     param($Name, $Value)
-    # $env:$Name = $value
-    # Process, User, Machine.
-    [Environment]::SetEnvironmentVariable($Name, $Value, 'User')
+   
+    $ErrorActionPreference = 'SilentlyContinue'
+    $isCloudShell = uname 
+    if ($isCloudShell) {
+        [Environment]::SetEnvironmentVariable($Name, $Value)
+    } else {
+        [Environment]::SetEnvironmentVariable($Name, $Value, 'User')
+    }
 }
 
 function env-get {
     param($Name)
-    # $env:$Name 
-    [Environment]::GetEnvironmentVariable($Name, 'User')
-}
-
-
-
-function cloud-env-set {
-    param($Name, $Value)
-    [Environment]::SetEnvironmentVariable($Name, $Value)
-}
-function cloud-env-get {
-    param($Name)
-    [Environment]::GetEnvironmentVariable($Name)
+  
+    $ErrorActionPreference = 'SilentlyContinue'
+    $isCloudShell = uname 
+    if ($isCloudShell) {
+        [Environment]::GetEnvironmentVariable($Name)
+    } else {
+        [Environment]::GetEnvironmentVariable($Name, 'User')
+    }
 }
