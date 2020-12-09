829b4df8f87ebec3176f02b7efc7908157b274cb
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-12-09 12:22:29 +0000

    Updated vm-connect.ps1

--- azure/vm-connect.ps1
+++ azure/vm-connect.ps1
@@ -1,12 +1,12 @@
 
-function connect-win-2020 {
+function connect-dev2020 {
     $server = "dev2020.westeurope.cloudapp.azure.com"
     $port   = "3389" 
 
     Start-Process "mstsc" -ArgumentList "/V:$server`:$port /fullscreen /admin " 
 }
 
-function connect-linux-2020 {
+function connect-ubuntu2020 {
     ssh sysadmin@ubuntu2020.westeurope.cloudapp.azure.com
 }
 
