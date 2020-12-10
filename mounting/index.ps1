45c2613eefa1d4a5f75d9ea2440128202d04a2f2
devops <dev@devops.org>
2020-12-10 12:47:50 +0100

    fix/delta

--- mounting/index.ps1
+++ mounting/index.ps1
@@ -1,15 +1,35 @@
 
+function kursus-hent {
+    $repo = "https://superusers-kursus@dev.azure.com/superusers-kursus/learning-powershell/_git/practical-powershell"
+    $stien = "C:\dev"
+    $path  = "$stien\practical-powershell" 
+    Set-Location $stien
+    if (-not(Test-Path $path) ) {
+        git clone $repo
+    }
+    return $path
+}
+
+function kursus-opdater {
+    $path = kursus-hent
+    cd $path
+    
+    git config pull.ff only
+    git pull
+}
+
 
 function kursus-mounting {
-    param(
-        $path = "C:\dev\practical-powershell\02-Udvidet-PowerShell"
-    )
+    
+    $path = kursus-hent 
+
     $config = @{
         Name = "kursus"
         PSProvider = "FileSystem"
         Root = $path
         Description = "Maps to my working folder."
     }
-    New-PSDrive @config -Scope Global
+    New-PSDrive @config -Scope Global -ErrorAction SilentlyContinue
+    cd kursus:
 }
 
