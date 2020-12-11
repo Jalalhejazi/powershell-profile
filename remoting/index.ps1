36616c80171780fc2343354213b9b498c6dee2d2
devops <dev@devops.org>
2020-12-11 11:53:44 +0100

    feature/ssh to windows

--- remoting/index.ps1
+++ remoting/index.ps1
@@ -53,4 +53,38 @@ function connect-to {
 function connect-to-linux {
     ssh user@machine0.superusers.dk 
 }
+
+
+function connect-to-windows {
+<#
+.DESCRIPTION
+    https://www.hanselman.com/blog/how-to-ssh-into-a-windows-10-machine-from-linux-or-windows-or-anywhere
+
+.EXAMPLE
+connect-to-windows -ComputerName 192.168.8.101 -UserName kursist -setup
+
+.EXAMPLE
+connect-to-windows -ComputerName dev2020.westeurope.cloudapp.azure.com -UserName sysadmin
+
+#>
+        
+    [CmdletBinding()]
+    param(
+        
+        [Parameter(Mandatory = $true)] [string] $ComputerName,
+        [Parameter(Mandatory = $true)] [string] $UserName,
+        [Parameter(Mandatory = $false)][switch] $setup    
+    )
     
+    $ErrorActionPreference = 'SilentlyContinue'
+
+    if ($setup) {
+        # The setup must be configured on the remote side (not client side)
+        Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+        Set-Service -Name sshd -StartupType 'Automatic'
+        Start-Service sshd
+        New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Program Files\PowerShell\7\pwsh.exe" -PropertyType String -Force
+    }
+    
+    ssh $UserName@$ComputerName
+}
