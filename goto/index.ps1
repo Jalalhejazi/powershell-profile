3d1ff7ba07704d553fcb6ff947067d526b86f555
devops <dev@devops.org>
2020-12-15 12:00:49 +0100

    fix/clean

--- goto/index.ps1
+++ goto/index.ps1
@@ -7,6 +7,10 @@ function goto-home {
   cd $ProfileHOME
 }
 
+function goto-dev {
+  cd c:\dev
+}
+
 $HOMEPAGE = 'https://www.superusers.dk'
 
 function goto-superusers {
@@ -29,24 +33,13 @@ function goto-powershell-profile {
   chrome 'https://dev.azure.com/superusers-kursus/PowerShell'
 }
 
-function goto-powershell-kursus {
-  chrome --incognito "https://dev.azure.com/superusers-kursus/learning-powershell"
-}
-
 function goto-google {
   param($search)
   chrome --incognito "https://www.google.com/search?newwindow=1&q=$search"
 }
 
-
 function goto-github-superusers {
   chrome 'https://github.com/SuperUsersDK/'
 }
-
-
-function goto-gist {
-  chrome 'https://gist.githubusercontent.com/Jalalhejazi/c850105a05df85207fbb21b7d1fc52be/raw/56dd185bde7e6feeeed0f1319ce09ce87da1c81f/event-grid-poc'
-}
-
 
 
