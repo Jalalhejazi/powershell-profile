77b52e1613c5baa091ab3e9981b266e16ba74987
dev <dev@superusers.dk>
2021-01-14 11:45:52 +0100

    fix/vm

--- azure/vm-connect.ps1
+++ azure/vm-connect.ps1
@@ -1,9 +1,26 @@
 
-function connect-dev2020 {
+
+function start-dev2020 {
+    az vm start -g "DEVOPS-ENGINEERS-DEV2020-543870" -n "dev2020" 
+}
+
+function stop-dev2020 {
+    az vm stop -g "DEVOPS-ENGINEERS-DEV2020-543870" -n "dev2020" 
+}
+
+function restart-dev2020 {
+    az vm restart -g "DEVOPS-ENGINEERS-DEV2020-543870" -n "dev2020" 
+}
+
+function connect-dev2020-rdp {
     $server = "dev2020.westeurope.cloudapp.azure.com"
     $port   = "3389" 
 
     Start-Process "mstsc" -ArgumentList "/V:$server`:$port /fullscreen /admin " 
+}
+
+function connect-dev2020-ssh {
+    ssh sysadmin@dev2020.westeurope.cloudapp.azure.com
 }
 
 function connect-ubuntu2020 {
