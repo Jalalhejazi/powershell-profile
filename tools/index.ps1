bcfb459fd2e4f252ae5367d2b368413b62466d5b
jalal <jalal@superusers.dk>
2021-10-14 20:25:13 +0200

    fix/tools

--- tools/index.ps1
+++ tools/index.ps1
@@ -1,34 +1,16 @@
-## Using scoop.sh (the missing apt-get install for windows)
-function scoop-install-web {
-    scoop install azure-cli 
-    scoop install python
-    scoop install dotnet-sdk
-    scoop install nodejs
-    scoop install vscode
-    scoop install devdocs
-    scoop install googlechrome
-    scoop install git 
-    scoop install gitignore 
+function please-make-my-life-easy {    
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SuperUsersDK/automation/master/Setup/setup-az-204.ps1'))
 
+}
+function scoop-install-web {
+    scoop install python
     pip install httpie
     
     npm install npm --global
     npm install @angular/cli @nestjs/cli typescript --global
+    npm install fkill-cli kill-tabs rimraf empty-trash-cli pen speed-test live-server http-server is-online-cli is-up-cli clipboard-cli --global
 }
-function scoop-install-utils {
-    scoop bucket add extras   
-    scoop install 7zip 
-    scoop install sudo 
-    scoop install touch
-    scoop install win32-openssh 
-    scoop install pwsh
-    scoop install concfg 
-    scoop install gow 
-    scoop install vim 
-    scoop install starship
-    scoop install windows-terminal
-}
-
 
 
 function scoop-update-all {
@@ -39,12 +21,6 @@ function scoop-update-all {
     scoop update 
     scoop update * --force --quiet
 }
-
-
-function scoop-help {
-   scoop  --help 
-}
-
 
 function scoop-list {
     scoop list
@@ -61,33 +37,3 @@ function scoop-info {
     param($appName)
     scoop info $appName
 }
-
-
-function scoop-uninstall {
-    param($appName)
-    scoop uninstall $appName
-}
-
-function install-semantic-version-tool {
-    dotnet tool install -g nbgv
-}
-
-
-function please-make-my-life-easy {
-    ## ref.: https://github.com/sindresorhus/awesome-nodejs
-     
-    npm install fkill-cli kill-tabs rimraf empty-trash-cli pen speed-test live-server http-server is-online-cli is-up-cli clipboard-cli --global
-}
-
-
-## vscode extensions TODO
-
-## npm i -g azure-functions-core-tools@3 --unsafe-perm true
-## code --install-extension ms-vscode.csharp 
-## code --install-extension ms-vscode.azurecli
-## code --install-extension ms-azuretools.vscode-azurefunctions
-## code --install-extension ms-vscode.azure-account
-## code --install-extension ms-azuretools.vscode-azurestorage
-## code --install-extension ms-vscode.powershell 
-
-
